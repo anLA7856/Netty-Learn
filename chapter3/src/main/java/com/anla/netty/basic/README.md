e7e62b59dfc7800c2d0d6d9f6952687c08666ce0
anLA7856 <luoan007@gmail.com>
2019-01-12 22:13:12 +0800

    fix readme

--- chapter3/src/main/java/com/anla/netty/basic/README.md
+++ chapter3/src/main/java/com/anla/netty/basic/README.md
@@ -1,9 +1,9 @@
-##介绍：
+/##介绍：
 该例子是一个传统的AIO运行例子，使用NIO基本类来构造程序.
 1. 运行`com.anla.netty.aio.TimeServer.java`
 2. 运行`com.anla.netty.aio.TimeClient.java`
 
-## NIO优点
+## NIO有点
 1. 通过java.util.concurrent.Futrue类来标识异步操作的结果
 2. 在执行异步操作的时候作为操作完成的回调
 
---
+++ chapter3/src/main/java/com/anla/netty/basic/TimeServer.java
@@ -0,0 +1,49 @@
+package com.anla.netty.nettyio;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * @user anLA7856
+ * @time 19-1-12 下午9:16
+ * @description
+ */
+public class TimeServer {
+    public void bind(int port) throws Exception{
+        // 配置NIO线程组
+        EventLoopGroup boosGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(boosGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 1024)
+                    .childHandler(new ChildChannelHandler());
+            // 绑定端口，同步等待成功
+            ChannelFuture future = bootstrap.bind(port).sync();
+            // 等待服务端监听端口关闭
+            future.channel().closeFuture().sync();
+        }finally {
+            // 优雅退出，释放线程池资源
+            boosGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        int port = 8080;
+
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+        }
+        new TimeServer().bind(port);
+    }
+}
