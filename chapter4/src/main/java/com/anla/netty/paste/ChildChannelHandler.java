7662b3410f600e3dd5482eba0f1e947721878e36
anLA7856 <luoan007@gmail.com>
2019-01-12 23:07:02 +0800

    增加 普通Netty导致粘包例子

---
+++ chapter4/src/main/java/com/anla/netty/paste/ChildChannelHandler.java
@@ -0,0 +1,15 @@
+package com.anla.netty.paste;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+
+/**
+ * @user anLA7856
+ * @time 19-1-12 下午9:22
+ * @description
+ */
+public class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
+    protected void initChannel(SocketChannel socketChannel) throws Exception {
+        socketChannel.pipeline().addLast(new TimeServerHandler());
+    }
+}
